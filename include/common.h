1289f159a9b60b92ee849ed4df1480e7af37734f
Zingo Andersen <zingo@zingo.org>
2023-01-17 02:04:51 +0100

    Set Maxspeed for a lap to 2h marathon speed
    e.g. If you run a lap faster then this it will not be counted.
    On a 7km lap it will be about 20min this is to not count shorter
    breaks in the aidstation but outside the BT scan range like going
    to the toilet and stuff.
    
    Signed-off-by: Zingo Andersen <zingo@zingo.org>

--- include/common.h
+++ include/common.h
@@ -13,8 +13,8 @@ extern ESP32Time rtc;
 //#define RACE_DISTANCE_LAP (400)
 #define RACE_LAPS 7
 
-
-#define MINIMUM_LAP_TIME_IN_SECONDS 1*60
+// Max speed is 2,83min/km (or 170s/km e.g. Marathon on 2h) on the lap, this is used to not count a new lap in less time then this
+#define MINIMUM_LAP_TIME_IN_SECONDS ((170*RACE_DISTANCE_LAP)/1000)
 
 extern bool raceOngoing;
 
