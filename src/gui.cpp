bb757428b2c0251b8255b70688308bd1fe5be0d2
Zingo Andersen <zingo@zingo.org>
2023-02-20 21:03:30 +0100

    Larger symbols in tabs
    Signed-off-by: Zingo Andersen <zingo@zingo.org>

--- src/gui.cpp
+++ src/gui.cpp
@@ -792,7 +792,7 @@ static void createGUITabRaceExtra(lv_obj_t * parent)
 
 void createGUI(void)
 {
-  static lv_obj_t * tv;
+  static lv_obj_t * tabView;
 
   fontNormal  = &lv_font_montserrat_16;
   fontLarge   = &lv_font_montserrat_24;
@@ -855,11 +855,10 @@ void createGUI(void)
 #define TAB_HIGHT 70
 #define TAB_TIME_WIDTH 200
 
-  tv = lv_tabview_create(lv_scr_act(), LV_DIR_TOP, TAB_HIGHT); // height of tab area
+  tabView = lv_tabview_create(lv_scr_act(), LV_DIR_TOP, TAB_HIGHT); // height of tab area
+  lv_obj_set_style_text_font(lv_scr_act(), fontTag, 0);
 
-  lv_obj_set_style_text_font(lv_scr_act(), fontNormal, 0);
-
-  lv_obj_t * tab_btns = lv_tabview_get_tab_btns(tv);
+  lv_obj_t * tab_btns = lv_tabview_get_tab_btns(tabView);
   lv_obj_set_style_pad_left(tab_btns, TAB_POS, 0);
 
 //  lv_obj_t * logo = lv_img_create(tab_btns);
@@ -875,6 +874,7 @@ void createGUI(void)
 
   lv_obj_t * label = lv_label_create(tab_btns);
   lv_label_set_text(label, "Crazy Capy Time");
+  lv_obj_add_style(label, &styleTitle, 0);
   lv_obj_add_style(label, &styleTextMuted, 0);
 //  lv_obj_align_to(label, logo, LV_ALIGN_OUT_RIGHT_BOTTOM, 10, 0);
   lv_obj_align(label, LV_ALIGN_OUT_LEFT_TOP, -TAB_POS + 10, 35);
@@ -888,58 +888,20 @@ void createGUI(void)
   lv_obj_set_width(btnTime,TAB_TIME_WIDTH);
   lv_obj_add_event_cb(btnTime, btnTime_event_cb, LV_EVENT_ALL, NULL);
 
-  labelRaceTime = lv_label_create(btnTime);          /*Add a label to the button*/
-  lv_label_set_text(labelRaceTime, "Start!");                     /*Set the labels text*/
+  labelRaceTime = lv_label_create(btnTime);  // Add a label to the button
+  lv_label_set_text(labelRaceTime, "Start!");
   lv_obj_center(labelRaceTime);
   lv_obj_add_style(labelRaceTime, &styleTime, 0);
 
-  tabRace = lv_tabview_add_tab(tv, "Race"); 
-  tabParticipants = lv_tabview_add_tab(tv, LV_SYMBOL_LIST );
-  lv_obj_t * t3 = lv_tabview_add_tab(tv, LV_SYMBOL_IMAGE );
+  tabRace = lv_tabview_add_tab(tabView, "Race"); 
+  tabParticipants = lv_tabview_add_tab(tabView, LV_SYMBOL_LIST );
+  lv_obj_t * tab3 = lv_tabview_add_tab(tabView, LV_SYMBOL_IMAGE );
 
   createGUITabRace(tabRace);
   createGUITabParticipant(tabParticipants);
-  createGUITabRaceExtra(t3);
+  createGUITabRaceExtra(tab3);
 }
 
-// Display callback to flush the buffer to screen
-void lvgl_displayFlushCallBack(lv_disp_drv_t *disp, const lv_area_t *area, lv_color_t *color_p)
-{
-  uint32_t w = (area->x2 - area->x1 + 1);
-  uint32_t h = (area->y2 - area->y1 + 1);
-
-#if (LV_COLOR_16_SWAP != 0)
-  gfx->draw16bitBeRGBBitmap(area->x1, area->y1, (uint16_t *)&color_p->full, w, h);
-#else
-  gfx->draw16bitRGBBitmap(area->x1, area->y1, (uint16_t *)&color_p->full, w, h);
-#endif
-
-  lv_disp_flush_ready(disp);
-}
-
-// Touchpad callback to read the touchpad
-void lvgl_touchPadReadCallback(lv_indev_drv_t *indev_driver, lv_indev_data_t *data)
-{
-  if (touch_has_signal())
-  {
-    if (touch_touched())
-    {
-      data->state = LV_INDEV_STATE_PR;
-
-      /*Set the coordinates*/
-      data->point.x = touch_last_x;
-      data->point.y = touch_last_y;
-    }
-    else if (touch_released())
-    {
-      data->state = LV_INDEV_STATE_REL;
-    }
-  }
-  else
-  {
-    data->state = LV_INDEV_STATE_REL;
-  }
-}
 
 void updateGUITime()
 {
@@ -951,60 +913,8 @@ void updateGUITime()
   }
 }
 
-void initLVGL()
-{
-  ESP_LOGI(TAG, "Setup GFX");
-  gfx->begin();
-  gfx->fillScreen(BLACK);
-#ifdef TFT_BL
-  pinMode(TFT_BL, OUTPUT);
-  digitalWrite(TFT_BL, HIGH);
-#endif
-
-  lv_init();
-  touch_init();
-  screenWidth = gfx->width();
-  screenHeight = gfx->height();
-//#ifdef ESP32
-  disp_draw_buf = (lv_color_t *)heap_caps_malloc(sizeof(lv_color_t) * screenWidth * screenHeight / 4, MALLOC_CAP_INTERNAL | MALLOC_CAP_8BIT);
-//#else
-//  disp_draw_buf = (lv_color_t *)malloc(sizeof(lv_color_t) * screenWidth * screenHeight / 4);
-//#endif
-  if (!disp_draw_buf)
-  {
-    ESP_LOGE(TAG, "LVGL disp_draw_buf allocate failed!");
-    return;
-  }
-  else
-  {
-    lv_disp_draw_buf_init(&draw_buf, disp_draw_buf, NULL, screenWidth * screenHeight / 4);
-
-    /* Initialize the display */
-    lv_disp_drv_init(&disp_drv);
-    /* Change the following line to your display resolution */
-    disp_drv.hor_res = screenWidth;
-    disp_drv.ver_res = screenHeight;
-    disp_drv.flush_cb = lvgl_displayFlushCallBack;
-    disp_drv.draw_buf = &draw_buf;
-//    disp_drv.rotated = LV_DISP_ROT_90;
-//    disp_drv.sw_rotate = 1;
-    lv_disp_drv_register(&disp_drv);
-
-    /* Initialize the (dummy) input device driver */
-    static lv_indev_drv_t indev_drv;
-    lv_indev_drv_init(&indev_drv);
-    indev_drv.type = LV_INDEV_TYPE_POINTER;
-    indev_drv.read_cb = lvgl_touchPadReadCallback;
-    lv_indev_drv_register(&indev_drv);
-
-    createGUI(); // MUST be done before adding participants
-    ESP_LOGI(TAG, "Setup GFX done");
-  }
-}
-
 void loopHandlLVGL()
 {
-
   msg_GFX msg;
   while ( xQueueReceive(queueGFX, &(msg), (TickType_t)0) == pdPASS)  // Don't block main loop just take a peek
   {
@@ -1083,4 +993,95 @@ void loopHandlLVGL()
   lv_timer_handler();
 }
 
+// ######################################################## GFX & Touch Driver stuff
+
+// Display callback to flush the buffer to screen
+void lvgl_displayFlushCallBack(lv_disp_drv_t *disp, const lv_area_t *area, lv_color_t *color_p)
+{
+  uint32_t w = (area->x2 - area->x1 + 1);
+  uint32_t h = (area->y2 - area->y1 + 1);
+
+#if (LV_COLOR_16_SWAP != 0)
+  gfx->draw16bitBeRGBBitmap(area->x1, area->y1, (uint16_t *)&color_p->full, w, h);
+#else
+  gfx->draw16bitRGBBitmap(area->x1, area->y1, (uint16_t *)&color_p->full, w, h);
+#endif
+
+  lv_disp_flush_ready(disp);
+}
+
+// Touchpad callback to read the touchpad
+void lvgl_touchPadReadCallback(lv_indev_drv_t *indev_driver, lv_indev_data_t *data)
+{
+  if (touch_has_signal())
+  {
+    if (touch_touched())
+    {
+      data->state = LV_INDEV_STATE_PR;
+
+      /*Set the coordinates*/
+      data->point.x = touch_last_x;
+      data->point.y = touch_last_y;
+    }
+    else if (touch_released())
+    {
+      data->state = LV_INDEV_STATE_REL;
+    }
+  }
+  else
+  {
+    data->state = LV_INDEV_STATE_REL;
+  }
+}
+
+void initLVGL()
+{
+  ESP_LOGI(TAG, "Setup GFX");
+  gfx->begin();
+  gfx->fillScreen(BLACK);
+#ifdef TFT_BL
+  pinMode(TFT_BL, OUTPUT);
+  digitalWrite(TFT_BL, HIGH);
+#endif
+
+  lv_init();
+  touch_init();
+  screenWidth = gfx->width();
+  screenHeight = gfx->height();
+//#ifdef ESP32
+  disp_draw_buf = (lv_color_t *)heap_caps_malloc(sizeof(lv_color_t) * screenWidth * screenHeight / 4, MALLOC_CAP_INTERNAL | MALLOC_CAP_8BIT);
+//#else
+//  disp_draw_buf = (lv_color_t *)malloc(sizeof(lv_color_t) * screenWidth * screenHeight / 4);
+//#endif
+  if (!disp_draw_buf)
+  {
+    ESP_LOGE(TAG, "LVGL disp_draw_buf allocate failed!");
+    return;
+  }
+  else
+  {
+    lv_disp_draw_buf_init(&draw_buf, disp_draw_buf, NULL, screenWidth * screenHeight / 4);
+
+    /* Initialize the display */
+    lv_disp_drv_init(&disp_drv);
+    /* Change the following line to your display resolution */
+    disp_drv.hor_res = screenWidth;
+    disp_drv.ver_res = screenHeight;
+    disp_drv.flush_cb = lvgl_displayFlushCallBack;
+    disp_drv.draw_buf = &draw_buf;
+//    disp_drv.rotated = LV_DISP_ROT_90;
+//    disp_drv.sw_rotate = 1;
+    lv_disp_drv_register(&disp_drv);
+
+    /* Initialize the (dummy) input device driver */
+    static lv_indev_drv_t indev_drv;
+    lv_indev_drv_init(&indev_drv);
+    indev_drv.type = LV_INDEV_TYPE_POINTER;
+    indev_drv.read_cb = lvgl_touchPadReadCallback;
+    lv_indev_drv_register(&indev_drv);
+
+    createGUI(); // MUST be done before adding participants
+    ESP_LOGI(TAG, "Setup GFX done");
+  }
+}
 
