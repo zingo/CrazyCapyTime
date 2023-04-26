7eea54fba280b723c365c4c0bb503f7827565eda
Zingo Andersen <zingo@zingo.org>
2023-04-26 20:05:34 +0200

    Remove unused config
    Signed-off-by: Zingo Andersen <zingo@zingo.org>

--- include/common.h
+++ include/common.h
@@ -9,13 +9,8 @@
 
 extern ESP32Time rtc;
 
-#define RACE_LAPS 6
-#define RACE_DISTANCE_TOTAL (42195)
-
 #define RACE_NAME_LENGTH        30
 #define PARTICIPANT_NAME_LENGTH 30
-
-#define DRAW_MAX_LAPS_IN_CHART (RACE_LAPS+10) //10 extra to allow some extra laps in case of errors or wrong config
 
 #define RACE_COUNTDOWN 15  // TODO use config setting instead of this
 extern uint32_t raceStartIn;
@@ -28,7 +23,7 @@ extern bool raceOngoing;
 // Stack size in words, not bytes.
 #define TASK_BT_STACK (6*1024)
 #define TASK_RACEDB_STACK (70*1024)
-#define TASK_GUI_STACK (70*1024)
+#define TASK_GUI_STACK (90*1024)
 
 extern TaskHandle_t xHandleBT;
 extern TaskHandle_t xHandleRaceDB;
